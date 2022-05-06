b0b26c7d6632eca708d97f0519732d79102cd15e
rphlmr <raphael@uaerom.com>
2022-05-06 21:47:28 +0200

    wait for page load

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -19,6 +19,8 @@ describe("smoke tests", () => {
     cy.findByLabelText(/password/i).type(loginForm.password);
     cy.findByRole("button", { name: /create account/i }).click();
 
+    cy.findByText("No notes yet");
+
     cy.findByRole("button", { name: /logout/i }).click();
     cy.findByRole("link", { name: /log in/i });
   });
