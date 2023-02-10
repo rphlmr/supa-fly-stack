01ec82b3f55c9d3f7e95efb0673c5de1aa039e5c
rphlmr <raphael@uaerom.com>
2023-02-10 17:23:41 +0100

    rework seed script

--- app/database/seed.server.ts
+++ app/database/seed.server.ts
@@ -16,60 +16,73 @@ const prisma = new PrismaClient();
 const email = "hello@supabase.com";
 
 const getUserId = async (): Promise<string> => {
-  const existingUserId = await supabaseAdmin.auth.admin
-    .listUsers()
-    .then(({ data }) => data.users.find((user) => user.email === email)?.id);
+  const userList = await supabaseAdmin.auth.admin.listUsers();
 
-  if (existingUserId) return existingUserId;
+  if (userList.error) {
+    throw userList.error;
+  }
 
-  const newUserId = await supabaseAdmin.auth.admin
-    .createUser({
-      email,
-      password: "supabase",
-      email_confirm: true,
-    })
-    .then(({ data }) => data.user?.id);
+  const existingUserId = userList.data.users.find(
+    (user) => user.email === email
+  )?.id;
 
-  if (newUserId) return newUserId;
+  if (existingUserId) {
+    return existingUserId;
+  }
 
-  throw new Error("Could not create or get user");
+  const newUser = await supabaseAdmin.auth.admin.createUser({
+    email,
+    password: "supabase",
+    email_confirm: true,
+  });
+
+  if (newUser.error) {
+    throw newUser.error;
+  }
+
+  return newUser.data.user.id;
 };
 
 async function seed() {
-  const id = await getUserId();
+  try {
+    const id = await getUserId();
 
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
+    // cleanup the existing database
+    await prisma.user.delete({ where: { email } }).catch(() => {
+      // no worries if it doesn't exist yet
+    });
 
-  const user = await prisma.user.create({
-    data: {
-      email,
-      id,
-    },
-  });
+    const user = await prisma.user.create({
+      data: {
+        email,
+        id,
+      },
+    });
 
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+    await prisma.note.create({
+      data: {
+        title: "My first note",
+        body: "Hello, world!",
+        userId: user.id,
+      },
+    });
 
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+    await prisma.note.create({
+      data: {
+        title: "My second note",
+        body: "Hello, world!",
+        userId: user.id,
+      },
+    });
 
-  console.log(`Database has been seeded. 🌱\n`);
-  console.log(
-    `User added to your database 👇 \n🆔: ${user.id}\n📧: ${user.email}\n🔑: supabase`
-  );
+    console.log(`Database has been seeded. 🌱\n`);
+    console.log(
+      `User added to your database 👇 \n🆔: ${user.id}\n📧: ${user.email}\n🔑: supabase`
+    );
+  } catch (cause) {
+    console.error(cause);
+    throw new Error("Seed failed 🥲");
+  }
 }
 
 seed()
